7ccebf18a52978209b280ddd553de6e7cdfc7136
Cássio Jandir Pagnoncelli <cassiopagnoncelli@gmail.com>
2025-11-26 21:11:22 -0300

    Bump version to 1.3.7 and export conform function in NAMESPACE

--- R/conform.R
+++ R/conform.R
@@ -5,6 +5,7 @@
 #' @param x Numeric vector to conform
 #' @param limit Absolute value limit (default: 1e15). Large values become NA.
 #' @return Numeric vector with invalid values replaced by NA
+#' @export
 conform <- function(x, limit = 1e5, na = NA_real_) {
   # Fast vectorized replacement: NaN, Inf, -Inf, and extreme values all become NA
   x[is.nan(x) | is.infinite(x) | abs(x) > limit] <- na
